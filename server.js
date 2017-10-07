4a02d52a1d2b991ec0dc3af7bdfb2a15d50ddd88
Kelsey Butler <kelseybutler@Kelseys-MacBook-Pro.local>
2017-10-07 11:15:16 -0500

    finalize project

--- server.js
+++ server.js
@@ -1,17 +1,10 @@
 const express = require('express');
-// const morgan = require('morgan');
+const morgan = require('morgan');
 
 const app = express();
-
 const blogPostsRouter = require('./blogPostsRouter');
 
-// app.use(morgan('common'));
-
-app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-  // res.sendFile(__dirname + '/views/index.html');
-});
+app.use(morgan('common'));
 
 app.use('/blog-posts', blogPostsRouter);
 
